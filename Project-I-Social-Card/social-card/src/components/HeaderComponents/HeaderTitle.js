e62e72ddd8d72a4ccd40b4f4d852bad55bb5a538
ropeks <ropeksmilioner@gmail.com>
2019-06-03 15:57:21 +0200

    add header component (with styles)

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className="title">
+            <strong>Lambda School</strong>
+            <span className="pale">@LambdaSchool</span>
+            <span className="pale">26 Jan</span>
+        </div>
+    );
+}
+
+export default HeaderTitle;
