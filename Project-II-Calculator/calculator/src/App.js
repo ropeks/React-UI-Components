618142c2f001de960278712796013c4af6c14006
ropeks <ropeksmilioner@gmail.com>
2019-06-04 15:29:37 +0200

    add button components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <CalculatorDisplay />
+      <div className="buttons">
+        <NumberButton />
+        <ActionButton />
+      </div>
     </div>
   );
 };
